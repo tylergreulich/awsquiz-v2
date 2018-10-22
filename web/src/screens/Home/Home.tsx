00eee7f165641109f55caa39aa83e02de55d9b29
tylergreulich <tgreul6@gmail.com>
2018-10-22 15:38:28 -0700

    interface

--- web/src/screens/Home/Home.tsx
+++ web/src/screens/Home/Home.tsx
@@ -2,14 +2,12 @@ import * as React from 'react';
 import { GoogleLogin, GoogleLoginResponse } from 'react-google-login';
 import axios from 'axios';
 
-interface HomeState {
-  isAuthenticated: boolean;
-}
+import { HomeState } from './home.interface';
 
 export class Home extends React.Component<{}, HomeState> {
   state: HomeState = { isAuthenticated: false };
 
-  onFailureHandler = (err: any) => {
+  onFailureHandler = (err: Error) => {
     console.log('[ERR]', err);
   };
 
